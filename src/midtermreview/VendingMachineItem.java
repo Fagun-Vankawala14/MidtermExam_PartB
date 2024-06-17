eec8032c09a4a69a2424053926ebf87634120acc
Simran kaur <kau16156@sheridancollege.ca>
2024-06-17 14:40:28 -0400

    code is completed for all the classes

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -8,29 +8,19 @@ package midtermreview;
 import java.util.Scanner;
 
 public class VendingMachineItem {
-    public double price;
+  
     
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
-    public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
     
-    public VendingMachineItem() {
-        // Constructor left blank intentionally
-    }
-    
-    public double getPrice() {
-        return price;
-    }
-    
-    public void setPrice(double givenPrice) {
-        price = givenPrice;
-    }
+        
+   
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
-        }
+//      
+   for(ItemType item: ItemType.values()){
+      if(item.isAvailable()){
+       System.out.println(item.getName()+item.getPrice());}
+   }
     }
     
     

822e9f64a14c01bf8c9b4b3ae9e5131a8cf35bf8
arshn <arshn@10.48.11.43>
2024-06-17 14:23:18 -0400

    Welcome to Vending Machine to get different types of candies.

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -10,6 +10,14 @@ import java.util.Scanner;
 public class VendingMachineItem {
     public double price;
     
+    private enum Candies{
+        CHOCOLATE BARS, SOUR CANDY, SOFT DRINKS, POTATO CHIPS
+    }
+    
+    private enum Prices{
+        1.50,1.20,1.80,2.00
+    }
+    
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
@@ -22,9 +30,12 @@ public class VendingMachineItem {
         return price;
     }
     
+    
     public void setPrice(double givenPrice) {
         price = givenPrice;
     }
+    
+  
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
@@ -33,10 +44,17 @@ public class VendingMachineItem {
         }
     }
     
+      public static void selectItem() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the number of the item you wish to purchase: ");
+        int itemNumber = sc.nextInt();
+    
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
+        selectItem();
+        
                 
 
       }

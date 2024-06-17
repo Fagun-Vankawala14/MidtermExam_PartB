41c00df8202233cd97517f9238730f16d892ce4b
Aryan KachhiyaPatel <kachhary.sheridancollege.ca>
2024-06-17 14:50:19 -0400

    Changes made in code.

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -26,18 +26,5 @@ public class VendingMachineItem {
         price = givenPrice;
     }
     
-    public static void displayMenu() {
-        System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
-        }
-    }
     
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        displayMenu();
-                
-
-      }
 }

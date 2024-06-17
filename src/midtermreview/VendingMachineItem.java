251449d9c9e14f891bd3965efb62df6656177779
kanoorde <kanoorde@sheridancollege.ca>
2024-06-17 14:32:36 -0400

    Final commit after refractoring the code as per two princiles of OO

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -7,8 +7,8 @@ package midtermreview;
 
 import java.util.Scanner;
 
-public class VendingMachineItem {
-    public double price;
+public abstract class VendingMachineItem {
+    private double price;
     
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
@@ -18,13 +18,9 @@ public class VendingMachineItem {
         // Constructor left blank intentionally
     }
     
-    public double getPrice() {
-        return price;
-    }
+    public abstract double getPrice();
     
-    public void setPrice(double givenPrice) {
-        price = givenPrice;
-    }
+    public abstract void setPrice();
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
@@ -50,7 +46,7 @@ public class VendingMachineItem {
             
             if (itemAvailability[indexOfItem]) 
             {
-                System.out.println("Here is your purchased item which is" + candies[indexOfItem]);
+                System.out.println("Here is your purchased item which is " + candies[indexOfItem]);
                 
                 itemAvailability[indexOfItem] = false; 
                 

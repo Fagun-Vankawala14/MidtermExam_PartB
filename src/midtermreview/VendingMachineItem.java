487af6753039f6e173288db973f584da172763ce
prabhdeepkaur <prabhdeepkaur@10.48.0.182>
2024-06-17 14:24:20 -0400

    This is my midterm Part B

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package midtermreview;
 
 import java.util.Scanner;
@@ -12,7 +7,7 @@ public class VendingMachineItem {
     
     public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
+    public static boolean[] itemAvailability = {true, true, true, true}; 
     
     public VendingMachineItem() {
         // Constructor left blank intentionally
@@ -28,16 +23,50 @@ public class VendingMachineItem {
     
     public static void displayMenu() {
         System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
+        for (int i = 0; i < candies.length; i++) {
+            String availability = itemAvailability[i] ? "Available" : "Unavailable";
+            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i] + " - " + availability);
         }
     }
     
+    public static void selectItem() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the quantity of items that you want to buy:");
+        int choice = sc.nextInt();
+        
+        if (choice < 1 || choice > candies.length) {
+            System.out.println("Invalid Quantity.Try again.");
+        } else {
+            int index = choice - 1;
+            if (itemAvailability[index]) {
+                System.out.println("You have selected " + candies[index] + " which costs $" + prices[index]);
+                System.out.println("Please insert the amount: ");
+                double amountInserted = sc.nextDouble();
+                
+                if (amountInserted >= prices[index]) {
+                    double change = amountInserted - prices[index];
+                    System.out.println("Thank you for your purchasing: Your change is $" + change);
+                    itemAvailability[index] = false; 
+                } else {
+                    System.out.println("Insufficient amount. Transaction cancelled.");
+                }
+            } else {
+                System.out.println("Sorry, the item is out of stock. Please select another item.");
+            }
+        }
+    }
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        displayMenu();
-                
-
-      }
+        while (true) {
+            displayMenu();
+            selectItem();
+            System.out.println("Do you want to purchase another item? (yes/no)");
+            String continueChoice = sc.next();
+            if (!continueChoice.equalsIgnoreCase("yes")) {
+                break;
+            }
+        }
+        System.out.println("Thank you for using this vending machine");
+    }
 }

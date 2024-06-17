d8b65530e23b667d7d7702929915ae513da4bbc4
Nupur Dhiman <dhimannu@sheridancollege.ca>
2024-06-17 13:53:54 -0400

    Assignment Complete

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class VendingMachineItem {
     public double price;
     
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
+    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips","maggi"};
     public static double[] prices = {1.50, 1.20, 1.80, 2.00};
     public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
     

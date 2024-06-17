6cdaaffe0283f37a2fe777070ad14becbb727321
Dev Ambaliya <ambalide@sheridancollege.ca>
2024-06-17 14:24:23 -0400

    CODE WORKING...

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -1,43 +1,39 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package midtermreview;
 
-import java.util.Scanner;
-
+/**
+ *
+ * @author freny
+ */
 public class VendingMachineItem {
-    public double price;
-    
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
-    public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
-    
-    public VendingMachineItem() {
-        // Constructor left blank intentionally
+
+    private final String name;
+    private final double price;
+    private boolean isAvailable;
+
+    public VendingMachineItem(String name, double price, boolean isAvailable) {
+        this.name = name;
+        this.price = price;
+        this.isAvailable = isAvailable;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
     public double getPrice() {
         return price;
     }
-    
-    public void setPrice(double givenPrice) {
-        price = givenPrice;
-    }
-    
-    public static void displayMenu() {
-        System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
-        }
-    }
-    
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        displayMenu();
-                
 
-      }
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
 }
+
+

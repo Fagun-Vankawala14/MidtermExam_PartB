7e7cbf4b1c4db70fa81684244d0199d89814db0d
RitikaSharma <ritika24@sheridancollege.ca>
2024-06-17 14:18:22 -0400

    adding select item method

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -8,36 +8,73 @@ package midtermreview;
 import java.util.Scanner;
 
 public class VendingMachineItem {
-    public double price;
     
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
-    public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
+
+    // Enum for candies
+    public enum Candies {
+        CHOCOLATEBAR, SOURCANDY, SOFTDRINK, POTATOCHIPS
+    }
+
+    // Enum for prices
+    public enum Prices {
+        CHOCOLATEBAR(1.50), SOURCANDY(1.20), SOFTDRINK(1.80), POTATOCHIPS(2.00);
+
+        private final double price;
+
+        Prices(double price) {
+            this.price = price;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+    private double price;
+    private Candies candies;
+    private Prices enumprice;
+
+
     
+    private static boolean[] itemAvailability = {true, true, true, true}; // Initially, all items are available
+
     public VendingMachineItem() {
         // Constructor left blank intentionally
     }
-    
-    public double getPrice() {
-        return price;
+
+     public double getPrice() {
+            return price;
+        }
+
+    public void setPrices(double givenPrices) {
+        price = givenPrices;
     }
-    
-    public void setPrice(double givenPrice) {
-        price = givenPrice;
-    }
-    
+
     public static void displayMenu() {
-        System.out.println("Welcome to the vending machine, here is a list of the possible candies:");
-        for (int i = 0; i < 4; i++) {
-            System.out.println((i+1) + ". " + candies[i] + " - $" + prices[i]);
+        System.out.println("list of the possible candies:");
+        for (Candies candies : Candies.values()) {
+            System.out.println(candies);
         }
     }
-    
-    
+
+    public static void selectItem(int Number) {
+        if (Number < 1 || Number > 4) {
+            System.out.println("Invalid selection item number from(1 to4)");
+            return;
+        }
+        if (itemAvailability[Number - 1]) {
+            System.out.println("Candies are " + Candies.values()[Number - 1] + Prices.values()[Number - 1].getPrice());
+            itemAvailability[Number - 1] = false; 
+        } else {
+            System.out.println("no candies"+ Candies.values()[Number - 1]);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         displayMenu();
-                
-
-      }
+        System.out.println("Please enter the number of the item(1to4):");
+        int select = sc.nextInt();
+        selectItem(select);
+    }
 }
+

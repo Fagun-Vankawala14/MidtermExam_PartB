1e07a58301e427d739bb7a06024555c725269b64
Arjun Singh Dhiilon <dhilarju@sheridancollege.ca>
2024-06-17 14:23:33 -0400

    Added the feature of encapsulation (oop concept)

--- src/midtermreview/VendingMachineItem.java
+++ src/midtermreview/VendingMachineItem.java
@@ -1,29 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package midtermreview;
 
 import java.util.Scanner;
 
 public class VendingMachineItem {
-    public double price;
+    private double price;
     
-    public static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
-    public static double[] prices = {1.50, 1.20, 1.80, 2.00};
-    public static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
+    private static String[] candies = {"chocolate bar", "sour candy", "soft drink", "potato chips"};
+    private static double[] prices = {1.50, 1.20, 1.80, 2.00};
+    private static boolean[] itemAvailability = {true, true, true, true}; // Initially all items are available
     
     public VendingMachineItem() {
         // Constructor left blank intentionally
     }
     
+   
     public double getPrice() {
         return price;
     }
     
     public void setPrice(double givenPrice) {
         price = givenPrice;
+    }
+    
+  
+    public static String[] getCandies() {
+        return candies;
+    }
+    
+    public static void setCandies(String[] newCandies) {
+        candies = newCandies;
+    }
+    
+
+    public static double[] getPrices() {
+        return prices;
+    }
+    
+    public static void setPrices(double[] newPrices) {
+        prices = newPrices;
+    }
+    
+   
+    public static boolean[] getItemAvailability() {
+        return itemAvailability;
+    }
+    
+    public static void setItemAvailability(boolean[] newItemAvailability) {
+        itemAvailability = newItemAvailability;
     }
     
     public static void displayMenu() {
@@ -33,19 +56,17 @@ public class VendingMachineItem {
         }
     }
     
-public static void selectItem(int itemNumber) {
-    if (itemNumber < 1 || itemNumber > candies.length) {
-        System.out.println("Invalid selection. Please select a valid item number.");
-    } else {
-        if (itemAvailability[itemNumber - 1]) {
-            System.out.println("You have selected " + candies[itemNumber - 1] + " for $" + prices[itemNumber - 1] + ".");
+    public static void selectItem(int itemNumber) {
+        if (itemNumber < 1 || itemNumber > candies.length) {
+            System.out.println("Invalid selection. Please select a valid item number.");
         } else {
-            System.out.println("Sorry, " + candies[itemNumber - 1] + " is not available.");
+            if (itemAvailability[itemNumber - 1]) {
+                System.out.println("You have selected " + candies[itemNumber - 1] + " for $" + prices[itemNumber - 1] + ".");
+            } else {
+                System.out.println("Sorry, " + candies[itemNumber - 1] + " is not available.");
+            }
         }
     }
-}
-
-    
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -53,5 +74,5 @@ public static void selectItem(int itemNumber) {
         System.out.print("Please enter the number of the item you wish to purchase: ");
         int itemNumber = sc.nextInt();
         selectItem(itemNumber);
-      }
+    }
 }
